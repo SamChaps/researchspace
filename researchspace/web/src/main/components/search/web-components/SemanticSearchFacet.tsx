b8c9a322893be4f3bde24d740d595eb87bbb2dac
Francis F <f.forget@polymtl.ca>
2019-12-02 11:28:04 -0500

    [Translation] - Hardcoded French string for Field Catalog

--- researchspace/web/src/main/components/search/web-components/SemanticSearchFacet.tsx
+++ researchspace/web/src/main/components/search/web-components/SemanticSearchFacet.tsx
@@ -187,7 +187,7 @@ class SemanticSearchFacetInner extends React.Component<InnerProps, State> {
           }),
           onClick: this.toggleFilter,
         },
-        this.state.showFacets ? 'Hide Filter' : 'Show Filter'
+        this.state.showFacets ? 'Masquer les filtres' : 'Afficher les filtres'
         )
       );
     } else {
